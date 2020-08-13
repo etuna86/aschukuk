5f60cfc9160c35a26fc88115ac2805d4648b1fb1
etuna86 <eroltuna86@outlook.com>
2020-08-13 16:43:40 +0300

    react add activity areas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,19 +5,23 @@ import './bootstrap/dist/css/bootstrap.css';
 //import './css/fonts.css';
 import './css/style.css';
 
-
+const imageURL='./';
 class App extends Component{
   constructor(props) {
     super(props);
     this.state={
         teamValue:-1,
+        activityValue:-1,
         teamValues:[],
-        teamMembers:[],
+        teamActivityAreas:[],
+        teams:[],
+        users:[],
         getTeamTrue:false,
     }
   }
 componentDidMount() {
 this.getTeamValues();
+console.warn("users",this.state.users);
 }
 getTeamValues=()=>{
     const getTeamValue=[
@@ -27,23 +31,67 @@ getTeamValues=()=>{
         {id:3,name:'Dava Ekibi'},
         {id:4,name:'İcra Ekibi'},
         ];
-    const teamMembers=[
+    const teams=[
       {id:0,name:'EKİP'},
       {id:1,name:'Yönetim'},
       {id:2,name:'Danışmanlık Ekibi'},
       {id:3,name:'Dava Ekibi'},
       {id:4,name:'İcra Ekibi'},
     ];
+  const users=[
+    {id:0, name:'Okan',surname:'Beygo',title:'Kurucu Ortak',imageSrc:'lawyer1.png',teams:[1,2,3],activities:[1,2,3]},
+    {id:1, name:'Denel Balli ',surname:'Kırali',title:'Ortak Avukat',imageSrc:'lawyer2.png',teams:[1,2,3],activities:[1,2,3]},
+    {id:2, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[1,2,3],activities:[1,2,3]},
+    {id:3, name:'Ela',surname:'Sarı',title:'Ortak Avukat',imageSrc:'lawyer4.png',teams:[1,2,3],activities:[1,2,3]},
+    {id:4, name:'Melis',surname:'Mercan',title:'Kıdemli Avukat',imageSrc:'lawyer5.png',teams:[1,2,3],activities:[1,2,3]},
+    {id:5, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[1,2,3],activities:[1,2,3]},
+    {id:6, name:'Okan',surname:'Beygo',title:'Kurucu Ortak',imageSrc:'lawyer1.png',teams:[4],activities:[4]},
+    {id:7, name:'Denel Balli ',surname:'Kırali',title:'Ortak Avukat',imageSrc:'lawyer2.png',teams:[4],activities:[1]},
+    {id:8, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[4],activities:[2]},
+    {id:9, name:'Ela',surname:'Sarı',title:'Ortak Avukat',imageSrc:'lawyer4.png',teams:[4],activities:[4]},
+    {id:10, name:'Melis',surname:'Mercan',title:'Kıdemli Avukat',imageSrc:'lawyer5.png',teams:[4],activities:[8]},
+    {id:11, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[4],activities:[10]},
+    {id:12, name:'Okan',surname:'Beygo',title:'Kurucu Ortak',imageSrc:'lawyer1.png',teams:[1],activities:[3]},
+    {id:13, name:'Denel Balli ',surname:'Kırali',title:'Ortak Avukat',imageSrc:'lawyer2.png',teams:[1],activities:[1]},
+    {id:14, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[1],activities:[3]},
+    {id:15, name:'Ela',surname:'Sarı',title:'Ortak Avukat',imageSrc:'lawyer4.png',teams:[1],activities:[2]},
+    {id:16, name:'Melis',surname:'Mercan',title:'Kıdemli Avukat',imageSrc:'lawyer5.png',teams:[1],activities:[11]},
+    {id:17, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[1],activities:[12]},
+    {id:18, name:'Okan',surname:'Beygo',title:'Kurucu Ortak',imageSrc:'lawyer1.png',teams:[0],activities:[1,2,3]},
+    {id:19, name:'Denel Balli ',surname:'Kırali',title:'Ortak Avukat',imageSrc:'lawyer2.png',teams:[0],activities:[1,2,3]},
+    {id:20, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[0],activities:[1,2,3]},
+    {id:21, name:'Ela',surname:'Sarı',title:'Ortak Avukat',imageSrc:'lawyer4.png',teams:[0],activities:[1,2,3]},
+    {id:22, name:'Melis',surname:'Mercan',title:'Kıdemli Avukat',imageSrc:'lawyer5.png',teams:[0],activities:[1,2,3]},
+    {id:23, name:'Levent',surname:'Yetkil',title:'Ortak Avukat',imageSrc:'lawyer3.png',teams:[0],activities:[1,2,3]},
+  ];
     const teamActivityAreas=[
-      {id:0,name:'EKİP'},
-      {id:1,name:'Yönetim'},
+      {id:0,name:'Bankacılık ve Finans'},
+      {id:1,name:'Borç Yapılandırma'},
       {id:2,name:'Danışmanlık Ekibi'},
       {id:3,name:'Dava Ekibi'},
-      {id:4,name:'İcra Ekibi'},
+      {id:4,name:'Türev İşlemler'},
+      {id:5,name:'Havacılık Finansmanı'},
+      {id:6,name:'Satın Alım Finansmanı'},
+      {id:7,name:'Proje Finansmanı ve Altyapı Yatırımları'},
+      {id:8,name:'Birleşme ve Devralmalar'},
+      {id:9,name:'Şirketler Hukuku'},
+      {id:10,name:'Gayrimenkul ve Kira Hukuku'},
+      {id:11,name:'Bilgi ve İletişim Teknolojileri'},
+      {id:12,name:'Özelleştirme'},
+      {id:13,name:'Kişisel Verilerin Korunması'},
+      {id:14,name:'Vergi'},
+      {id:15,name:'Fikri ve Sınai Mülkiyet Hukuku'},
+      {id:16,name:'Taşımacılık Hukuku'},
+      {id:17,name:'İdare Hukuku'},
+      {id:18,name:'Kamulaştırma Hukuku'},
+      {id:19,name:'Uyuşmazlık Çözümü'},
+      {id:20,name:'Ceza Hukuku'},
     ]
     this.setState({
       teamValues:getTeamValue,
-      teamMembers:teamMembers,
+      teamActivityAreas:teamActivityAreas,
+      teams:teams,
+      users:users,
     })
 }
 changeTeamValues=(e)=>{
@@ -57,20 +105,33 @@ changeTeamValues=(e)=>{
   }, 1000);
 
   if (this.state.teamValue === -1){
-    return this.state.teamMembers.map((member) =>
+    return this.state.teams.map((member) =>
         <option  value={member.id}  >{member.name}</option>
     );
   }
 
 }
-
+  changeActivityAreas=(e)=>{
+    this.setState({
+      activityValue:e.target.value,
+      getTeamTrue: true,
+    })
+    setTimeout(() => {
+      this.setState({ getTeamTrue: false });
+    }, 1000);
+  }
   render(){
     let getValuesData= this.state.teamValues.map((value) =>
         <option  value={value.id}  >{value.name}</option>
   );
+
+    let getActivityData= this.state.teamActivityAreas.map((value) =>
+        <option  value={value.id}  >{value.name}</option>
+    );
     let getTeams=[];
+    let getUsers=[];
     if (this.state.teamValue == -1) {
-      getTeams = this.state.teamMembers.map((member) =>
+      getTeams = this.state.teams.map((member) =>
           <div className="team-section">
             <h1>{member.name}</h1>
             <div className="row">
@@ -146,75 +207,67 @@ changeTeamValues=(e)=>{
     }
     else{
       getTeams =  <div className="team-section">
-            <h1>{this.state.teamMembers[this.state.teamValue].name}</h1>
-            <div className="row">
-              <div className="col-md-2">
-                <a href="#">
-                  <div className="lawyers-box">
-                    <div className="lawyers-box-img">
-                      <img src={require('./img/lawyer/lawyer1.png')} alt="" />
-                    </div>
-                    <h3>Okan Beygo</h3>
-                    <span>Kurucu Ortak</span>
-                  </div>
-                </a>
+            <h1>{this.state.teams[this.state.teamValue].name}</h1>
+              <div className="row">
+                {  this.state.users.map((user,i)=> {
+                  console.warn("user.teams.includes(this.state.teamValue): ", user.teams.includes(this.state.teamValue));
+                  console.warn("user.teams: ",user.teams )
+                  console.warn("this.state.teamValue: ",this.state.teamValue )
+                  let getTeams=user.teams;
+                  let getActivityAreas=user.activities;
+                  let teamValue=this.state.teamValue;
+                  let activityValue=this.state.activityValue;
+                  console.warn("teamValue",teamValue);
+                  var found=false;
+                  var foundtwo=false;
+                  getTeams.find(function (element) {
+                    if (element == teamValue)
+                        return found=true;
+                  else
+                    return found=false;
+                  });
+
+                  getActivityAreas.find(function (element) {
+                    if (element == activityValue)
+                      return foundtwo=true;
+                    else
+                      return foundtwo=false;
+                  });
+                  if(found === true){
+                    if (this.state.activityValue== -1 ){
+                      return (
+                          <div key={i} className="col-md-2">
+                            <a href="#">
+                              <div className="lawyers-box">
+                                <div className="lawyers-box-img">
+                                  <img src={require('./img/lawyer/'+user.imageSrc)} alt="" />
+                                </div>
+                                <h3>{user.name+ " " +user.surname}</h3>
+                                <span>{user.title}</span>
+                              </div>
+                            </a>
+                          </div>
+                      )
+                    }else{
+                        if (foundtwo==true){
+                          return (
+                              <div key={i} className="col-md-2">
+                                <a href="#">
+                                  <div className="lawyers-box">
+                                    <div className="lawyers-box-img">
+                                      <img src={require('./img/lawyer/'+user.imageSrc)} alt="" />
+                                    </div>
+                                    <h3>{user.name+ " " +user.surname}</h3>
+                                    <span>{user.title}</span>
+                                  </div>
+                                </a>
+                              </div>
+                          )
+                        }
+                    }
+                   }
+                }) }
               </div>
-              <div className="col-md-2">
-                <a href="#">
-                  <div className="lawyers-box">
-                    <div className="lawyers-box-img">
-                      <img src={require('./img/lawyer/lawyer2.png')} alt="" />
-                    </div>
-                    <h3>DENEL BALCI KIRALİ</h3>
-                    <span>ORTAK AVUKAT</span>
-                  </div>
-                </a>
-              </div>
-              <div className="col-md-2">
-                <a href="#">
-                  <div className="lawyers-box">
-                    <div className="lawyers-box-img">
-                      <img src={require('./img/lawyer/lawyer3.png')} alt="" />
-                    </div>
-                    <h3>LEVENT YETKİL</h3>
-                    <span>ORTAK AVUKAT</span>
-                  </div>
-                </a>
-              </div>
-              <div className="col-md-2">
-                <a href="#">
-                  <div className="lawyers-box">
-                    <div className="lawyers-box-img">
-                      <img src={require('./img/lawyer/lawyer4.png')} alt="" />
-                    </div>
-                    <h3>ELA SARI</h3>
-                    <span>ORTAK AVUKAT</span>
-                  </div>
-                </a>
-              </div>
-              <div className="col-md-2">
-                <a href="#">
-                  <div className="lawyers-box">
-                    <div className="lawyers-box-img">
-                      <img src={require('./img/lawyer/lawyer5.png')} alt="" />
-                    </div>
-                    <h3>MELİS MERCAN</h3>
-                    <span>KIDEMLİ AVUKAT</span>
-                  </div>
-                </a>
-              </div>
-              <div className="col-md-2">
-                <a href="#">
-                  <div className="lawyers-box">
-                    <div className="lawyers-box-img">
-                      <img src={require('./img/lawyer/lawyer3.png')} alt="" />
-                    </div>
-                    <h3>LEVENT YETKİL</h3>
-                    <span>ORTAK AVUKAT</span>
-                  </div>
-                </a>
-              </div>
-            </div>
           </div>
 
     }
@@ -230,35 +283,16 @@ changeTeamValues=(e)=>{
                       {getValuesData}
                     </select>
 
-                    <select className="form-control" id="select-activity-areas">
-                      <option>FAALİYET ALANLARI</option>
-                      <option>Bankacılık ve Finans</option>
-                      <option>Borç Yapılandırma</option>
-                      <option>Türev İşlemler</option>
-                      <option>Havacılık Finansmanı</option>
-                      <option>Satın Alım Finansmanı</option>
-                      <option>Proje Finansmanı ve Altyapı Yatırımları</option>
-                      <option>Birleşme ve Devralmalar</option>
-                      <option>Şirketler Hukuku</option>
-                      <option>Gayrimenkul ve Kira Hukuku</option>
-                      <option>Bilgi ve İletişim Teknolojileri</option>
-                      <option>Özelleştirme</option>
-                      <option>Kişisel Verilerin Korunması</option>
-                      <option>Vergi</option>
-                      <option>İş Hukuku</option>
-                      <option>Fikri ve Sınai Mülkiyet Hukuku</option>
-                      <option>Taşımacılık Hukuku</option>
-                      <option>İdare Hukuku</option>
-                      <option>Kamulaştırma Hukuku</option>
-                      <option>Uyuşmazlık Çözümü</option>
-                      <option>Ceza Hukuku</option>
+                    <select value={this.state.activityValue} onChange={this.changeActivityAreas} className="form-control" id="select-activity-areas">
+                      <option value={-1}>FAALİYET ALANLARI</option>
+                      {getActivityData}
                     </select>
                     <div className="all-team-select">
                       TÜMÜ
                     </div>
                   </div>
                 </div>
-                {this.state.getTeamTrue  ? <div class={"bd-example"}><div className="text-center"><div className="spinner-border text-primary" role="status">
+                {this.state.getTeamTrue  ? <div className={"bd-example"}><div className="text-center"><div className="spinner-border text-primary" role="status">
                   <span className="sr-only">Loading...</span>
                 </div></div></div> : getTeams}
               </div>
